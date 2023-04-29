6f4eed4a9e5dfafff6b2840eed099669a21ee486
Fabrice Kopf <contact@fabricekopf.fr>
2023-04-29 08:48:12 +0200

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# Génère des mots de passes cryptés
+
+En développement quand la création de user n'est pas encore codée, et qu'il faut un premier user.
+Ou un mot de passe oublié en développement et qu'il n'y a aucun moyen de le récupérer ou modifier autrement qu'en dur dans la bdd.
+
+* Fabrice Kopf
---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+
+if(isset($_POST['userPass'])){
+    $userPass = $_POST['userPass'];
+    $hachPass = password_hash($userPass, PASSWORD_BCRYPT);
+
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Génération de mots de passe cryptés</title>
+</head>
+<body>
+<section>
+    <form action="#" method="post">
+        <div>
+            <label for="userPass">Créez un mot de passe crypté:</label>
+            <input type="password" placeholder="Mot de passe" id="userPass" name="userPass" required>
+            <button type="submit">VALIDER</button>
+        </div>
+
+                <?php
+                    if(isset($hachPass)){
+                        echo "<div><p>".$userPass." : </p><p>".$hachPass."</p></div>";
+                    }
+                ?>
+
+    </form>
+</section>
+</body>
+</html>
